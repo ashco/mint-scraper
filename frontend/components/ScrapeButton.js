c0bc1b9f1bc8e40229c89d05b4d90588c5b90365
Ashton <ashchristie@gmail.com>
2019-05-22 00:10:43 -0700

    added in scrape button

---
+++ frontend/components/ScrapeButton.js
@@ -0,0 +1,24 @@
+﻿/* eslint-disable class-methods-use-this */
+import React from 'react';
+
+const targetUrl =
+  process.env.NODE_ENV === 'production'
+    ? 'http://192.168.86.72:4000'
+    : 'http://localhost:4000';
+
+export default class ScrapeButton extends React.Component {
+  async handleClick() {
+    const res = await fetch(`${targetUrl}/scrape`);
+    const message = await res.text();
+
+    alert(message);
+  }
+
+  render() {
+    return (
+      <button type="button" onClick={this.handleClick}>
+        Scrape!
+      </button>
+    );
+  }
+}
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,12 +1,14 @@
 ﻿import Page from '../components/Page';
 import Data from '../components/Data';
 import AuthButton from '../components/AuthButton';
+import ScrapeButton from '../components/ScrapeButton';
 
 export default function Home() {
   return (
     <Page>
       <h2>Wealth Tracker</h2>
       <AuthButton />
+      <ScrapeButton />
       <Data />
     </Page>
   );
